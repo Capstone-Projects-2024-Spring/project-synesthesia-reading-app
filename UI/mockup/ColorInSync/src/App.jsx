3afcc4b0d68a75d6ccd5e0980530e70b656a3adc
Elijah Bigham <elijahbigham@gmail.com>
2024-02-12 22:18:54 +0000

    setup of UI workspace - CNS hello world with Vite, React, and Tailwind

---
+++ UI/mockup/ColorInSync/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  //const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-gray-500 flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-purple-300">Color in Sync</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
