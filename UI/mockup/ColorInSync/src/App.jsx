db3953feaa5b4ed4dea86330d0dea9e582afe80d
Elijah Bigham <elijahbigham@gmail.com>
2024-02-14 22:30:13 +0000

    toying around with dynamic colors

--- UI/mockup/ColorInSync/src/App.jsx
+++ UI/mockup/ColorInSync/src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  //const [count, setCount] = useState(0);
+  const [R, setR] = useState(0);
+  const [G, setG] = useState(0);
+  const [B, setB] = useState(0);
 
   return (
     <>
-      <div className="bg-gray-500 flex items-center justify-center h-screen">
+      <div className="bg-gray-200 flex items-center justify-center h-screen">
         <h1 className="text-3xl font-bold text-purple-300">Color in Sync</h1>
+        <button>Red</button>
+        <input></input>
       </div>
     </>
   );
