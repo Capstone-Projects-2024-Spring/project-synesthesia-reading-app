444fc8fcc50b4de9d8c3b1e6b2a731ebb8f5a91d
Elijah Bigham <elijahbigham@gmail.com>
2024-02-26 22:19:29 -0500

    Google API key is not obfuscated in .env

--- UI/mockup/ColorInSync/src/main.jsx
+++ UI/mockup/ColorInSync/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="1043925707973-48qeooqngh05ohquc587963ounidds28.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OATH_KEY}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
