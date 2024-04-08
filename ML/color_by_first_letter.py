1c4c00409c50931bd2e21559355454f65ac45a39
alyssatownsend <tuh25732@temple.edu>
2024-04-08 18:26:42 -0400

    Update color_by_first_letter.py
    - functionc color creates a word-color map

--- ML/color_by_first_letter.py
+++ ML/color_by_first_letter.py
@@ -1,11 +1,12 @@
 '''
-OUTPUT: page_object.json in output folder
+OUTPUT: page_object.json in UI public folder
 '''
 
 class ColorByFirstLetter():
     def __init__(self):
-        self.unique_words = None
+        self.unique_words = ['the','swift','red','fox','jumped','over','lazy','brown','dog']
         self.word_color_map = None
+        #self.generate_words()
 
     def generate_words(self):
         from random_word import RandomWords
@@ -13,15 +14,18 @@ class ColorByFirstLetter():
         words = list(range(100))
         for word in words:
             words[word] = r.get_random_word()
+        print(' '.join(words))
+        self.unique_words = words
 
-if __name__ == '__main__':
+
+
     def get_grapheme_map(self):
         '''
         put in code to call API for grapheme-word map here and interpret it as a Pandas DataFrame
         in the meantime, gives an example grapheme-color
         '''
         import pandas as pd
-        return pd.read_csv('Maras_Alphabet.csv')
+        return pd.read_csv('Maras_Alphabet.csv').set_index('Letter').to_dict('index')
 
 
     def color(self):
@@ -29,27 +33,17 @@ if __name__ == '__main__':
         from IPython.display import display
 
         grapheme_map = self.get_grapheme_map()
-        grapheme_map = grapheme_map.set_index(['Letter'])
-        display(grapheme_map)
 
-        # create an empty word-color map and add words to it
-        word_color_map = pd.DataFrame(data=None, columns=['Word','Red','Green','Blue'])
-        word_color_map['Word'] = self.unique_words
 
-        # add color values for each word in map
-        for index, row in word_color_map.iterrows():
-            word = word_color_map.at[index, 'Word']
-            row = grapheme_map.loc[word[0].upper()]
-            word_color_map.at[index, 'Red'] = row.at['R']
-            word_color_map.at[index, 'Green'] = row.at['G']
-            word_color_map.at[index, 'Blue'] = row.at['B']
+        # create an empty word-color map
+        word_color_map = {}
 
-        word_color_map = word_color_map.set_index('Word')
+        # add word-rgb value pairs for each unique word
+        for word in self.unique_words:
+            rgb = grapheme_map.get(word[0].upper())
+            word_color_map[word] = 'rgb({},{},{})'.format(rgb.get('R'), rgb.get('G'), rgb.get('B'))
+
         self.word_color_map = word_color_map
-        return word_color_map.to_json(orient='table')
-
-
-
 
 
 
@@ -60,7 +54,9 @@ if __name__ == '__main__':
 
 
     c = ColorByFirstLetter()
-    c.generate_words()
+    c.color()
+
+
 
 #%%
 
