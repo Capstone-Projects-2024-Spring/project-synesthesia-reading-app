59608c874f3a5cbba52fe37b6dcb19975fdb2c19
alyssatownsend <tuh25732@temple.edu>
2024-04-08 18:54:15 -0400

    coloring script for demo 2
    - color_by_first_letter.py in ML folder creates a list of random words and a word-color map
    - this is output as a JSON file in UI/public folder

--- ML/color_by_first_letter.py
+++ ML/color_by_first_letter.py
@@ -4,20 +4,27 @@ OUTPUT: page_object.json in UI public folder
 
 class ColorByFirstLetter():
     def __init__(self):
-        self.unique_words = ['the','swift','red','fox','jumped','over','lazy','brown','dog']
+        self.words = None
+        self.unique_words = None
         self.word_color_map = None
-        #self.generate_words()
+        self.generate_words()
+        self.get_unique_words()
+        self.color()
+        self.create_page_object()
 
+    # geenrating a list of words for mock-up purposes
     def generate_words(self):
         from random_word import RandomWords
         r = RandomWords()
         words = list(range(100))
         for word in words:
             words[word] = r.get_random_word()
-        print(' '.join(words))
-        self.unique_words = words
 
+        self.words = words
 
+    # get unique set of words from self.words, i.e. the tokenized list of words in a page
+    def get_unique_words(self):
+        self.unique_words = list(set(self.words))
 
     def get_grapheme_map(self):
         '''
@@ -27,11 +34,8 @@ class ColorByFirstLetter():
         import pandas as pd
         return pd.read_csv('Maras_Alphabet.csv').set_index('Letter').to_dict('index')
 
-
+    # creates a word-color map and assigns it to self.word_color_map
     def color(self):
-        import pandas as pd
-        from IPython.display import display
-
         grapheme_map = self.get_grapheme_map()
 
 
@@ -46,15 +50,32 @@ class ColorByFirstLetter():
         self.word_color_map = word_color_map
 
 
+    # creates a json object of the list of words and the word-color map and writes it to a file
+    def create_page_object(self):
+        import json
+
+        # combine list of unique words & word-color map into one object
+        page_object = {
+            'words': self.words,
+            'word_color_map': self.word_color_map
+        }
+
+        # convert to JSON
+        page_object = json.dumps(page_object)
+
+        # write to file
+        with open("../UI/public/page_object.json", "w") as outfile:
+            outfile.write(page_object)
+
+
+
 
 
 
 if __name__ == '__main__':
-    words = ['the','swift','red','fox','jumped','over','lazy','brown','dog']
-
 
     c = ColorByFirstLetter()
-    c.color()
+
 
 
 
