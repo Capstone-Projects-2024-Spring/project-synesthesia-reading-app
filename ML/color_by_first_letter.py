c6192c9dfbd3da3a58596dd94e42da030af6e760
alyssatownsend <tuh25732@temple.edu>
2024-04-19 16:36:38 -0400

    parses grapheme-color map from json
    - created a tester file for color_by_first_letter.py
    - passed test to get a grapheme-color map from the color profile json given to the ColorByFirstLetter object

--- ML/color_by_first_letter.py
+++ ML/color_by_first_letter.py
@@ -4,40 +4,33 @@ OUTPUT: page_object.json in UI public folder
 
 class ColorByFirstLetter():
     def __init__(self, color_profile, text):
-        self.color_profile_json = color_profile
-        self.words_json = text
+        self.color_json = color_profile
+        self.text_json = text
+
+        self.grapheme_map = None
+        self.exception_map = None
         self.unique_words = None
         self.word_color_map = None
+
+        self.parse_json()
+
         #self.get_unique_words()
         #self.color()
         #self.create_page_object()
 
     def parse_json(self):
         import json
-        json_dict = json.loads(self.json)
-        print(json_dict)
+        color_profile = json.loads(self.color_json)
+        self.grapheme_map = color_profile["grapheme-color map"]
+        self.exception_map= color_profile["exceptions-color map"]
 
-    # geenrating a list of words for mock-up purposes
-    def generate_words(self):
-        from random_word import RandomWords
-        r = RandomWords()
-        words = list(range(100))
-        for word in words:
-            words[word] = r.get_random_word()
-
-        self.words = words
 
     # get unique set of words from self.words, i.e. the tokenized list of words in a page
     def get_unique_words(self):
         self.unique_words = list(set(self.words))
 
     def get_grapheme_map(self):
-        '''
-        put in code to call API for grapheme-word map here and interpret it as a Pandas DataFrame
-        in the meantime, gives an example grapheme-color
-        '''
-        import pandas as pd
-        return pd.read_csv('Maras_Alphabet.csv').set_index('Letter').to_dict('index')
+        return self.grapheme_map
 
     # creates a word-color map and assigns it to self.word_color_map
     def color(self):
@@ -77,11 +70,7 @@ class ColorByFirstLetter():
 
 
 
-if __name__ == '__main__':
-    color_profile = {"grapheme-color map": {"A":  "rgb(230,16,16)", "B":  "rgb(29,26,214)", "C":  "rgb(250,247,65)", "D":  "rgb(5,29,153)", "E": "rgb(11,176,16)", "F": "rgb(227,16,16)", "G": "rgb(16,163,21)", "H": "rgb(255,239,15)", "I": "rgb(216,242,242)", "J": "rgb(255,227,46)", "K": "rgb(255,248,33)", "L": "rgb(245,29,29)", "M": "rgb(252,10,10)", "N": "rgb(71,4,148)", "O": "rgb(97,186,250)", "P": "rgb(250,240,50)", "Q": "rgb(170,197,242)", "R": "rgb(145,67,230)", "S": "rgb(255,252,69)", "T": "rgb(255,246,69)", "U": "rgb(46,162,230)", "V": "rgb(198,200,247)", "W": "rgb(8,39,196)", "X": "rgb(80,80,82)", "Y": "rgb(255,242,0)", "Z": "rgb(197,228,232)"}, "exceptions-color map":  {}}
-    tokenized_text = ["The ", "over", "weight ", "red ", "fox ", "jumped ", "over ", "the ", "sleeping ", "brown ", "dog", "."]
 
-    c = ColorByFirstLetter(color_profile, tokenized_text)
 
 
 
