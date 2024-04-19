ebc3419e8723dace83872565e373890e1657bed7
alyssatownsend <tuh25732@temple.edu>
2024-04-19 16:08:53 -0400

    coloring object requires color profile & text

--- ML/color_by_first_letter.py
+++ ML/color_by_first_letter.py
@@ -3,14 +3,19 @@ OUTPUT: page_object.json in UI public folder
 '''
 
 class ColorByFirstLetter():
-    def __init__(self):
-        self.words = None
+    def __init__(self, color_profile, text):
+        self.color_profile_json = color_profile
+        self.words_json = text
         self.unique_words = None
         self.word_color_map = None
-        self.generate_words()
-        self.get_unique_words()
-        self.color()
-        self.create_page_object()
+        #self.get_unique_words()
+        #self.color()
+        #self.create_page_object()
+
+    def parse_json(self):
+        import json
+        json_dict = json.loads(self.json)
+        print(json_dict)
 
     # geenrating a list of words for mock-up purposes
     def generate_words(self):
@@ -73,8 +78,11 @@ class ColorByFirstLetter():
 
 
 if __name__ == '__main__':
+    color_profile = {"grapheme-color map": {"A":  "rgb(230,16,16)", "B":  "rgb(29,26,214)", "C":  "rgb(250,247,65)", "D":  "rgb(5,29,153)", "E": "rgb(11,176,16)", "F": "rgb(227,16,16)", "G": "rgb(16,163,21)", "H": "rgb(255,239,15)", "I": "rgb(216,242,242)", "J": "rgb(255,227,46)", "K": "rgb(255,248,33)", "L": "rgb(245,29,29)", "M": "rgb(252,10,10)", "N": "rgb(71,4,148)", "O": "rgb(97,186,250)", "P": "rgb(250,240,50)", "Q": "rgb(170,197,242)", "R": "rgb(145,67,230)", "S": "rgb(255,252,69)", "T": "rgb(255,246,69)", "U": "rgb(46,162,230)", "V": "rgb(198,200,247)", "W": "rgb(8,39,196)", "X": "rgb(80,80,82)", "Y": "rgb(255,242,0)", "Z": "rgb(197,228,232)"}, "exceptions-color map":  {}}
+    tokenized_text = ["The ", "over", "weight ", "red ", "fox ", "jumped ", "over ", "the ", "sleeping ", "brown ", "dog", "."]
 
-    c = ColorByFirstLetter()
+    c = ColorByFirstLetter(color_profile, tokenized_text)
+
 
 
 
