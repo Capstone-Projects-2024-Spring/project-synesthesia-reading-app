6d3dac92708abcc97d5da757619c77387c60e073
AndrewDKahn <49370482+AndrewDKahn@users.noreply.github.com>
2024-04-27 15:58:17 +0000

    namespace stuff

--- colorInSync/routes/api.php
+++ colorInSync/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\Api\v1\DocumentController;
+use App\Http\Controllers\Api\v1\ColorProfileController;
 
 
 
