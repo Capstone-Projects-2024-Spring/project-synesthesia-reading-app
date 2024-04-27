27c476ecd1bf078fb0f066c75f37d75f5e8c2efd
AndrewDKahn <49370482+AndrewDKahn@users.noreply.github.com>
2024-04-27 16:30:53 +0000

    anotha one

--- colorInSync/routes/api.php
+++ colorInSync/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\Api\v1\DocumentController;
-use app\Http\Controllers\Api\v1\ColorProfileController;
+use app\Http\Controllers\DocumentController;
+use app\Http\Controllers\ColorProfileController;
 
 
 
@@ -24,7 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1', 'namespace' => 'app\Http\Controllers\Api\v1'], function(){
-    Route::get('colorProfile', [ColorProfileController::class, 'show']);
-    Route::get('/document/{id}', [DocumentController::class, 'show']);
-});
+Route::get('colorProfile', [ColorProfileController::class, 'show']);
+Route::get('/document/{id}', [DocumentController::class, 'show']);
+
+#Route::group(['prefix' => 'v1', 'namespace' => 'app\Http\Controllers\Api\v1'], function(){
+#   Route::get('colorProfile', [ColorProfileController::class, 'show']);
+#   Route::get('/document/{id}', [DocumentController::class, 'show']);
+#});
