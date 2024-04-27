2a1f0bdac5860e9c98ba8856da8afba93b4262b7
AndrewDKahn <49370482+AndrewDKahn@users.noreply.github.com>
2024-04-27 16:58:03 +0000

    anotha one

--- colorInSync/routes/api.php
+++ colorInSync/routes/api.php
@@ -28,6 +28,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 #Route::get('/document/{id}', [DocumentController::class, 'show']);
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\v1'], function(){
-    Route::get('colorProfile', [ColorProfileController::class, 'show']);
+    Route::get('/colorProfile', [ColorProfileController::class, 'show']);
     Route::get('/document/{id}', [DocumentController::class, 'show']);
 });
