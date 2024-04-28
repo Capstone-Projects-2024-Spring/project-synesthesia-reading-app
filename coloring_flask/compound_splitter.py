f01a33a9afd8149c29d85d8b266e215e68adb207
alyssatownsend <tuh25732@temple.edu>
2024-04-28 16:03:02 -0400

    splits a word into 3 components
    - passes test_split_layered_compound()

--- coloring_flask/compound_splitter.py
+++ coloring_flask/compound_splitter.py
@@ -27,20 +27,29 @@ class CompoundSplitter():
     def get_split_words_list(self):
         return self.split_words_list
     
-    def split(self, word):
+    def split(self, word, baseIsWord, temp_word_list):
         max_index = len(word)
         for index, char in enumerate(word):
             # split into 2 components at index
             left_component = word[0:max_index-index]
             right_component = word[max_index-index: max_index]
 
-            if (index > 0) and (len(left_component) > 1):
-                if self.is_word(left_component) and self.is_word(right_component):
-                    self.split_words_list.append(left_component)
-                    self.split_words_list.append(right_component)
-                    return None
-            
-        self.split_words_list.append(word)
+            if (index > 0) and (len(left_component) > 1) and (len(right_component) > 1):
+                if self.is_word(left_component):
+                    if self.is_word(right_component):
+                        for word in temp_word_list:
+                            self.split_words_list.append(word)
+
+                        self.split_words_list.append(left_component)
+                        self.split(right_component, baseIsWord=True, temp_word_list=[])
+                        return True
+                    else:
+                        temp_word_list.append(left_component)
+                        if self.split(right_component, baseIsWord=False, temp_word_list=temp_word_list):
+                            return True
+        
+        if baseIsWord:
+            self.split_words_list.append(word)
 
 
 
--- coloring_flask/test_compound_splitter.py
+++ coloring_flask/test_compound_splitter.py
@@ -8,16 +8,23 @@ def test_loads_dict():
 def test_is_word():
     assert (s.is_word('anopheles') and s.is_word('fun') and not(s.is_word('macbook')))
 
-def test_splits_compouond_word():
-    s.split('anyone')
+def test_splits_compound_word():
+    s.split('anyone', True, [])
     actual = s.get_split_words_list()
     print(actual)
     correct = ['any', 'one']
     assert actual == correct
     
 def test_doesnt_split_not_compound():
-    s.split('angelic')
+    s.split('angelic', True, [])
     actual = s.get_split_words_list()
     correct = ['any', 'one', 'angelic']
+    print(actual)
+    assert actual == correct
+
+def test_split_layered_compound():
+    s.split('nevertheless', True, [])
+    actual = s.get_split_words_list()
+    correct = ['any', 'one', 'angelic', 'never', 'the', 'less']
     print(actual)
     assert actual == correct
